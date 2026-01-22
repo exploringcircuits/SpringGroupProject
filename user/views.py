3a7fda20d233b02c4311572340c7d36ea411e517
Will Cook Jr <cookw3@lasalle.edu>
2026-01-21 20:15:17 -0500

    basic login view upon terminal load /user/register

--- user/views.py
+++ user/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 
+
+@login_required
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request, 'home.html')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']  
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except:
+                messages.error(request, 'Username already exists.')
+        else:
+            messages.error(request, 'Passwords do not match.')
+    return render(request, 'registration/register.html')
