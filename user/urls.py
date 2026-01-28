6351d259af8efb6b957076575f00c5fd91a75dee
Will Cook Jr <cookw3@lasalle.edu>
2026-01-27 20:09:32 -0500

    login page takes users to home page

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home, register_view
+from .views import home, register_view, login_view, logout_view
 urlpatterns = [
     path('', home, name='home'),
     path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
 ]
--- user/views.py
+++ user/views.py
@@ -13,13 +13,13 @@ def home(request):
 
 def register_view(request):
     if request.method == 'POST':
-        username = request.POST['username']  
+        username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
-        
         if password == confirm_password:
             try:
-                user = User.objects.create_user(username=username, password=password)
+                user = User.objects.create_user(username=username, password=password, email=email)
                 user.save()
                 login(request, user)
                 return redirect('home')
@@ -28,3 +28,34 @@ def register_view(request):
         else:
             messages.error(request, 'Passwords do not match.')
     return render(request, 'registration/register.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username= username, password= password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, 'registration/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+from django.core.mail import send_mail
+from django.http import HttpResponse
+
+def test_email(request):
+    send_mail(
+        'Test Email',
+        'This is a test email from Django.',
+        'datonayomide@gmail.com',  # From email (must match EMAIL_HOST_USER)
+        ['datonayomide@example.com'],  # Replace with your actual recipient email
+        fail_silently=False,  # Make Django raise errors if email fails
+    )
+    return HttpResponse('Test email sent!')
